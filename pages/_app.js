8ec4cf031eee235dad12904ee01b51f9a0836e27
vixalien <37999241+vixalien@users.noreply.github.com>
2020-08-08 02:11:16 +0200

    minimum scale is normal

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=5, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=yes, viewport-fit=cover"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=yes, viewport-fit=cover"
         />
       </Head>
       <Theme.Provider value={{ toggleTheme, theme }}>
